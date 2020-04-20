69ecd785069d52def5b9606d65e09702fa82b186
Rodrigo Freitas <rodfrei@gmail.com>
2020-04-20 17:39:41 -0300

    Style Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25062007?s=460&u=31730c5330c320326193adb680154c5f908bf744&v=4"
+            alt="Rodrigo Freitas"
+          />
+          <div>
+            <strong>rocktseat/unform</strong>
+            <p>Descrição do repositorio</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25062007?s=460&u=31730c5330c320326193adb680154c5f908bf744&v=4"
+            alt="Rodrigo Freitas"
+          />
+          <div>
+            <strong>rocktseat/unform</strong>
+            <p>Descrição do repositorio</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/25062007?s=460&u=31730c5330c320326193adb680154c5f908bf744&v=4"
+            alt="Rodrigo Freitas"
+          />
+          <div>
+            <strong>rocktseat/unform</strong>
+            <p>Descrição do repositorio</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
